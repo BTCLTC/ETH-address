db36724d07df1cacc60f4f35f2cfedc659fd11c8
yunweb <admin@github.xyz>
2021-12-02 16:48:22 +0800

    update code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 extern crate ethkey;
 extern crate hex;
 extern crate rand;
-extern crate rustc_serialize;
 
 use ethkey::{Generator, Random};
 
 use crate::file::file_operation;
 
-use rustc_serialize::hex::FromHex;
-
 mod file;
 
 fn main() {
     let prefix = "00000000";
-    let hex = &prefix.from_hex().unwrap();
 
     loop {
         let keypair = Random.generate().unwrap();
 
-        if keypair.address().starts_with(hex) {
+        let prefix_addr = &keypair.address().to_string()[..8];
+        if prefix_addr == prefix {
             println!("Your new ethereum vanity address: {:? }", keypair.address());
             println!("Your new private key: {}", keypair.secret());
             file_operation(keypair.address().to_string(), keypair.secret().to_string());
